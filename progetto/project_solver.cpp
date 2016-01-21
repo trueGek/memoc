50869ee716fbf2ec0bb4d7c718ff327b432aeb2a
trueGek <gquadrio@gail.com>
2016-01-21 22:47:03 +0100

    Finito di commentare e sistemare il codice del del solver. Resta da sistemare ora il codice del main del cplex!!!

--- progetto/project_solver.cpp
+++ progetto/project_solver.cpp
@@ -235,24 +235,38 @@ float project_solver::getSimAnnealing(int multistart, bool random){
 
 vector<int> project_solver::findBestN(vector<int> sol){
 
-    vector<int> neigh = sol;
+	vector<int> newSol = sol;			// Definisco newSol impostandola uguale a sol
 
-    for (int s1 = 1; s1 < n-1; s1++){  
-        for (int s2 = s1+1; s2 < n; s2++){
 
-            neigh = sol;
-            for (int i = s1, j = s2; i < j; i++, j--){
-                float ppp = neigh[i];
-                neigh[i] = sol[j];
-                neigh[j] = ppp;
-            }
+	// Cicli for che mi serviranno per calcolare il vicinato
 
-            if (evaluate(neigh) < evaluate(sol)){
-                return neigh;
-            }
-        }
-    }
-    return neigh;
+	for (int pos1 = 1; pos1 < n-1; pos1++){  
+		for (int pos2 = pos1+1; pos2 < n; pos2++){
+
+		        newSol = sol;			// Ad ogni nuovo ciclo for ridefinisco newSol a sol
+
+
+			// Queso ciclo for serve per invertire gli elementi contenuti all'interno dell'intervallo pos1 - pos2
+
+        		for (int i = pos1, j = pos2; i < j; i++, j--){
+        	        	float ppp = newSol[i];
+        	        	newSol[i] = sol[j];
+        	        	newSol[j] = ppp;
+        	    	}
+
+
+			// if che mi va a valutare se la nuova soluzione newSol è migliore della vecchia soluzione sol
+
+        	   	if (evaluate(newSol) < evaluate(sol)){
+
+				// Se entro nel ciclo significa che la nuova soluzione calcolata è migliore e terminerò così restituendo newSol, altrimenti continuo a ciclare
+
+        	        	return newSol;
+        	    	}
+        	}
+    	}
+
+	return newSol;
 }
 
 
@@ -310,66 +324,78 @@ main(int argc, char* argv[]){
 
 	}
 
+
+	// Vado ad inserire nel file di output a che dataset si sta riferendo
+
 	ofstream SaveFile( "results_new_vers.txt", ios_base::app);
-		SaveFile << endl << endl << argv[1] << endl << endl;
-		SaveFile.close();
+	SaveFile << endl << endl << argv[1] << endl << endl;
+	SaveFile.close();	
+
+	char* c = argv[1];					// Prendo il nome del file in input
+	Reader* initializator = new Reader(c);			// Do in pasto al reader il nuovo file in input
 	
+	int test = 10;						// Numero di test da effettuare per ogni dataset
 
-	
-	char* c = argv[1];
-	Reader* initializator = new Reader(c);
-	
-	int test = 10;
-	int x = 0;
-	int* p_x = &x;
+	std::chrono::system_clock::time_point start, end;	// Mi definisco lo strumento per rilevare il tempo
+    	
 
-	std::chrono::system_clock::time_point start, end;
-    
-    	for (int p = 1; p <= initializator->problems; p++){
+	// Cicli for principali, mi serviranno per andare a calcolare le varie soluzioni che otterrò dai vari dataset
 
-		SA_sol = 0;
-		BSA_sol = DBL_MAX;
-		WSA_sol = DBL_MIN;
-		MSA_sol = 0;
-		TSA_sol.clear();
-		//vector<double> TSA_sol (10);  		
-		SA_count = 1;
+    	for (int p = 1; p <= initializator->problems; p++){	// Il primo ciclo for opererà per p volte con p <= al numero di problemi per ogni dataset
 
-		int n_nodes = initializator->nodes;
-		vector<vector<float> > xy;
-     	 	initializator->getNextProblem(xy);
-      	  	cout << "problema " << p << endl;
-        	float total_time = 0.0f;
+		SA_sol = 0;					// Azzeramento della soluzione corrente del Simulated Annealing
+		BSA_sol = DBL_MAX;				// Azzeramento della migliore soluzione del Simulated Annealing
+		WSA_sol = DBL_MIN;				// Azzeramento della peggiore soluzione del Simulated Annealing
+		MSA_sol = 0;					// Azzeramento della media delle soluzioni del Simulated Annealing
+		TSA_sol.clear();				// Azzeramento del vettore delle soluzioni ottenute dal Simulated Annealing
+		SA_count = 1;					// Azzeramento della del contatore che utilizzerò nel Simulated Annealing
+
+		int n_nodes = initializator->nodes;		// Definisco il numero di nodi del problema corrente ottenendolo dall'inizializator
+		vector<vector<float> > xy;			// Definisco il vettore xy
+     	 	initializator->getNextProblem(xy);		// Valorizzo il vettore xy con i valori del nuovo problema ottenuto dall'inizializator
+		float total_time = 0.0f;      	  		// Definizione e valorizzazione del tempo totale impiegato per la risoluzione di un dataset: somma di tutti i tempi
+
+		double best_sol = DBL_MAX;			// Definizione di best_sol ovvero la migliore soluzione globale trovata fino ad un dato momento 
+		double worst_sol = DBL_MIN;			// Definizione di worst_sol ovvero la peggiore soluzione globale trovata fino ad un dato momento
+		double med_sol = 0;				// Definizione di med_sol ovvero la media delle soluzioni trovate fino a questo momento
+		vector<double> total_sol (10);  		// Definizione del vettore di tutte le soluzioni trovate fino ad ora
+
+		// Stampe per la visualizzazione dei risultati a console
+
+		cout << "problema " << p << endl;		
 		cout << "Numero nodi: " << n_nodes << endl;
-
-		double best_sol = DBL_MAX;
-		double worst_sol = DBL_MIN;
-		double med_sol = 0;
-		vector<double> total_sol (10);  	
         	
-		for (int tt = 1; tt <= test; tt++){         		
+		for (int tt = 1; tt <= test; tt++){				// Il secondo ciclo for opererà per tt volte con tt <= al numero di test da effettuare per ogni dataset
 
-			project_solver* solver = new project_solver(xy);
-		        start = std::chrono::system_clock::now();
-			float sol = solver->getSimAnnealing(1,true);
-			
-			//float sol = solver->getLocalSearch();		        
-			med_sol = med_sol + sol;
-			end = std::chrono::system_clock::now();  
-			float cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0f;
-			//float cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();			
-			total_time = total_time + cur_time;
-			
-			SA_count++;
-			
-			total_sol[tt-1] = sol;
+			project_solver* solver = new project_solver(xy);	// Definisco un nuovo solver su cui poi opererò per ottenere i risultati dei due metodi implementati	
+		        
+			start = std::chrono::system_clock::now();		// Inizio a rilevare il tempo
 
-			if (sol <= best_sol){
+			float sol = solver->getSimAnnealing(1,true);		// Richiamo il metodo per avere la soluzione del Simulated Annealing			
+			//float sol = solver->getLocalSearch();			// Richiamo il metodo per avere la soluzione della Local Search
+	        
+			med_sol = med_sol + sol;				// Aggiorno la somma delle soluzioni
+
+			end = std::chrono::system_clock::now();  		// Termino il rilevamento del tempo
+
+			float cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0f;	// Calcolo del tempo corrente
+			total_time = total_time + cur_time;			// Calcolo del tempo totale
+			
+			SA_count++;						// Incremento il numero delle iterazioni totali da effettuare per un dataset
+			total_sol[tt-1] = sol;					// Inserisco nel vettore total_sol la soluzione corrente
+
+
+			// Vado a calcolare la soluzione migliore trovata fino ad ora
+
+			if (sol <= best_sol){	
 
 				best_sol = sol;
 
 			}
 			
+
+			// Vado a calcolare la soluzione peggiore trovata fino ad ora
+
 			if (sol >= worst_sol){
 
 				worst_sol = sol;
@@ -380,10 +406,12 @@ main(int argc, char* argv[]){
 
 		}
 
-		// Ritorno in output i vari valori ottenuti eseguendo l'algoritmo
 
-		double Variance_SA = 0;
-		double Variance = 0;
+		double Variance_SA = 0;						// Definizione della varianza del Simulated Annealing
+		double Variance = 0;						// Definizione della varianza generale
+
+
+		// Ciclo for per il calcolo della varianza
 
 		for (int i = 0; i<test; i++){
 				
@@ -393,14 +421,18 @@ main(int argc, char* argv[]){
 		}
 
 
+		// Aggiornamento dei vari valori necessari per la raccolta dati
+
 		MSA_sol /= test;
 		Variance_SA /= test;
 		med_sol /= test;
 		total_time /= test;
 		Variance /= test;
 
-		cout << endl;
 
+		// Operazioni di output a console per visualizzare i dati
+
+		cout << endl;
 		cout << "Average_Time " << p << ": " << total_time << endl;
 		
 		cout << endl;
@@ -421,6 +453,7 @@ main(int argc, char* argv[]){
 		cout << endl;
 
 		
+		// Operazioni per la scrittura su file dei dati calcolati
 
 		ofstream SaveFile( "results_new_vers.txt", ios_base::app);
 		SaveFile << p << "\t"  << total_time << "\t" << BSA_sol << "\t" << WSA_sol << "\t" << MSA_sol<< "\t" << Variance_SA << "\t" <<best_sol << "\t" << worst_sol << "\t" << med_sol << "\t" << Variance << endl;
